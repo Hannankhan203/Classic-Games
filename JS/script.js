9cb6c4f24ab9dcb1c930768a48db0b9895eba014
Hannankhan203 <ak1818375@gmail.com>
2024-11-03 20:05:21 +0500

    Fixed some UX/UI of main page

--- JS/script.js
+++ JS/script.js
@@ -1,43 +1,49 @@
 // Calling elements from html file
 let h1 = document.querySelector("h1");
 let games = document.querySelectorAll(".game");
-let modeBtn = document.querySelector("#mode");
+let modeBtn1 = document.querySelector("#mode1");
+let modeBtn2 = document.querySelector("#mode2");
 let body = document.querySelector("body");
+modeBtn2.classList.add("toggle");
 
 // Setting default mode to light mode
 let mode = "Light mode";
 
 // Dark mode function
 const darkMode = () => {
-    modeBtn.innerText = "Activate Light Mode!";
-    body.classList.add("body-dark-mode");
-    h1.classList.add("dark-mode-bg");
-    modeBtn.classList.add("dark-mode-bg");
-    games.forEach((game) => {
-        game.classList.add("dark-mode-bg");
+  // modeBtn.innerText = "Activate Light Mode!";
+  body.classList.add("body-dark-mode");
+//   modeBtn2.classList.add("dark-mode-bg");
+  games.forEach((game) => {
+      game.classList.add("dark-mode-bg");
     });
-}
+    modeBtn1.classList.add("toggle");
+    modeBtn2.classList.remove("toggle");
+};
 
 // Light mode function
 const lightMode = () => {
-    modeBtn.innerText = "Activate Dark Mode!";
-    body.classList.remove("body-dark-mode");
-    h1.classList.remove("dark-mode-bg");
-    modeBtn.classList.remove("dark-mode-bg");
-    games.forEach((game) => {
-        game.classList.remove("dark-mode-bg");
+  // modeBtn.innerText = "Activate Dark Mode!";
+  body.classList.remove("body-dark-mode");
+  h1.classList.remove("dark-mode-bg");
+  //   modeBtn.classList.remove("dark-mode-bg");
+  games.forEach((game) => {
+      game.classList.remove("dark-mode-bg");
     });
-}
+    modeBtn2.classList.add("toggle");
+    modeBtn1.classList.remove("toggle");
+};
 
 // Mode toggle function
 const toggleMode = () => {
-    if (mode === "Light mode"){
-        mode = "Dark mode";
-        darkMode();
-    } else {
-        mode = "Light mode";
-        lightMode();
-    }
-}
+  if (mode === "Light mode") {
+    mode = "Dark mode";
+    darkMode();
+  } else {
+    mode = "Light mode";
+    lightMode();
+  }
+};
 
-modeBtn.addEventListener("click", toggleMode);
+modeBtn1.addEventListener("click", toggleMode);
+modeBtn2.addEventListener("click", toggleMode);
