2fd422290510cbb0da9258640b53b0142c75f734
Hannankhan203 <ak1818375@gmail.com>
2024-12-03 22:37:30 +0500

    Fixed UI for tic tac toe

--- Tic Tac Toe/JS/script.js
+++ Tic Tac Toe/JS/script.js
@@ -1,235 +1,131 @@
-// Calling elements from HTML file
-let boxes = document.querySelectorAll(".box");
-let resetBtn = document.querySelector("#reset-btn");
-let newBtn = document.querySelector("#new-btn");
-let msgContainer = document.querySelector(".msg-container");
-let msg = document.querySelector("#msg");
-let h1 = document.querySelector("h1");
-let container = document.querySelector(".container");
-let drawMsg = document.querySelector("#draw-msg");
-let msgContDraw = document.querySelector(".msg-container-draw");
-let playerOne = document.querySelector(".player-one");
-let playerTwo = document.querySelector(".player-two");
-let body = document.querySelector("body");
-let modeBtn1 = document.querySelector("#mode1");
-let modeBtn2 = document.querySelector("#mode2");
-let back = document.querySelector(".back");
-let backLink = document.querySelector(".back-link");
-let gameChange = document.querySelector(".change-game-container");
-let change = document.querySelector(".change-game");
-let changeLink = document.querySelector(".change-link");
+// Selecting necessary elements from the HTML
+const boxes = document.querySelectorAll(".box");
+const resetBtn = document.querySelector("#reset-btn");
+const newBtn = document.querySelector("#new-btn");
+const msgContainer = document.querySelector(".msg-container");
+const msg = document.querySelector("#msg");
+const h1 = document.querySelector("h1");
+const container = document.querySelector(".container");
+const drawMsg = document.querySelector("#draw-msg");
+const msgContDraw = document.querySelector(".msg-container-draw");
+const playerOne = document.querySelector(".player-one");
+const playerTwo = document.querySelector(".player-two");
+const body = document.querySelector("body");
+const themeCheckbox = document.querySelector("#theme-checkbox");
+const back = document.querySelector(".back");
+const check = document.querySelector(".check");
+const botn = document.querySelector(".botn");
 
-playerTwo.classList.remove("turn");
-gameChange.classList.add("hide");
-resetBtn.classList.remove("reset-btn-2");
-modeBtn2.classList.add("toggle");
+// Initial setup: light mode
+body.classList.add("light-mode");
+resetBtn.classList.add("light-mode");
+back.classList.add("light-mode");
+boxes.forEach((box) => {
+    box.classList.add("light-mode");
+});
+botn.classList.add("light-mode");
+check.classList.add("light-mode");
+newBtn.classList.add("light-mode");
 
 
-
-let mode = "Light mode";
-
-
-const updatePlayerClasses = () => {
-    if (mode === "Dark mode") {
-        playerOne.classList.remove("turn"); // Clear light mode's turn highlighting
-        playerTwo.classList.remove("turn"); // Clear light mode's turn highlighting
-        
-        if (turnO) {
-            playerOne.classList.remove("dark-turn");
-            playerTwo.classList.add("dark-turn");
-        } else {
-            playerOne.classList.add("dark-turn");
-            playerTwo.classList.remove("dark-turn");
-        }
-    } else {
-        playerOne.classList.remove("dark-turn"); // Clear dark mode's turn highlighting
-        playerTwo.classList.remove("dark-turn"); // Clear dark mode's turn highlighting
-        
-        if (turnO) {
-            playerOne.classList.add("turn");
-            playerTwo.classList.remove("turn");
-        } else {
-            playerOne.classList.remove("turn");
-            playerTwo.classList.add("turn");
-        }
-    }
-}
-
-const darkMode = () => {
-    // modeBtn.innerText = "Activate Light Mode!";
+// Toggle between light mode and dark mode
+function toggleMode() {
+    body.classList.toggle("dark-mode");
+    resetBtn.classList.toggle("dark-mode");
+    back.classList.toggle("dark-mode");
     boxes.forEach((box) => {
-        box.classList.add("darkBox");
+        box.classList.toggle("dark-mode");
     });
-    resetBtn.classList.add("darkLayout", "darkLayout2");
-    newBtn.classList.add("darkLayout", "darkLayout2");
-    msg.classList.add("darkLayout");
-    h1.classList.add("darkLayout");
-    drawMsg.classList.add("darkLayout");
-    playerOne.classList.add("darkLayout");
-    playerTwo.classList.add("darkLayout");
-    body.classList.add("body-dark");
-    modeBtn1.classList.add("toggle");
-    modeBtn2.classList.remove("toggle");
-    back.classList.add("darkLayout2");
-    backLink.classList.add("darkLayout");
-    change.classList.add("darkLayout2");
-    changeLink.classList.add("darkLayout");
-    updatePlayerClasses();
+    playerOne.classList.toggle("dark-mode");
+    playerTwo.classList.toggle("dark-mode");
+    h1.classList.toggle("dark-mode");
+    msgContainer.classList.toggle("dark-mode");
+    msgContDraw.classList.toggle("dark-mode");
+    check.classList.toggle("dark-mode");
+    botn.classList.toggle("dark-mode");
+newBtn.classList.toggle("dark-mode");
+
 }
 
-const lightMode = () => {
-    // modeBtn.innerText = "Activate Mode Mode!";
-    boxes.forEach((box) => {
-        box.classList.remove("darkBox");
-    });
-    resetBtn.classList.remove("darkLayout", "darkLayout2");
-    newBtn.classList.remove("darkLayout", "darkLayout2");
-    msg.classList.remove("darkLayout");
-    h1.classList.remove("darkLayout");
-    drawMsg.classList.remove("darkLayout");
-    playerOne.classList.remove("darkLayout");
-    playerTwo.classList.remove("darkLayout");
-    body.classList.remove("body-dark");
-    modeBtn2.classList.add("toggle");
-    modeBtn1.classList.remove("toggle");
-    back.classList.remove("darkLayout2");
-    backLink.classList.remove("darkLayout");
-    change.classList.remove("darkLayout2");
-    changeLink.classList.remove("darkLayout");
-    updatePlayerClasses();
-}
+// Add event listener for theme toggle
+themeCheckbox.addEventListener("click", toggleMode);
 
+// Game logic
+let turnO = true; // Player O starts
 
-const toggleMode = () => {
-    if (mode === "Light mode") {
-        mode = "Dark mode";
-        darkMode();
-        updatePlayerClasses();
-    } else {
-        mode = "Light mode";
-        lightMode();
-        updatePlayerClasses();
-    }
-    updatePlayerClasses();
-}
-
-
-modeBtn1.addEventListener("click", toggleMode);
-modeBtn2.addEventListener("click", toggleMode);
-
-
-// Setting who will play first
-let turnO = true;
-
-// patterns to check the winning scenario
 const winPatterns = [
-    [0, 1, 2], 
-    [3, 4, 5], 
-    [6, 7, 8], 
-    [0, 3, 6], 
-    [1, 4, 7], 
-    [2, 5, 8], 
-    [0, 4, 8], 
-    [2, 4, 6]
+    [0, 1, 2], [3, 4, 5], [6, 7, 8],
+    [0, 3, 6], [1, 4, 7], [2, 5, 8],
+    [0, 4, 8], [2, 4, 6]
 ];
 
-// Reset or new game fuction for button
+const updatePlayerClasses = () => {
+    playerOne.classList.toggle("turn", !turnO);
+    playerTwo.classList.toggle("turn", turnO);
+};
+
 const resetGame = () => {
     turnO = true;
-    enableBoxes();
+    boxes.forEach((box) => {
+        box.disabled = false;
+        box.innerText = "";
+    });
     msgContainer.classList.add("hide");
+    msgContDraw.classList.add("hide");
     container.classList.remove("hide");
+    back.classList.remove("hide");
+    playerOne.classList.add("turn");
+    playerTwo.classList.remove("turn");
     h1.classList.remove("hide");
     resetBtn.classList.remove("hide");
-    msgContDraw.classList.add("hide");
-    playerTwo.classList.remove("turn");
-    playerOne.classList.add("turn");
-    back.classList.remove("hide");  
-    gameChange.classList.add("hide");
-    resetBtn.classList.remove("reset-btn-2");
-    gameChange.classList.add("change-game-2");
     updatePlayerClasses();
 };
 
-// Clicking and alternating the turns then disabling the clicked buttons then checking the winner
-boxes.forEach((box) => {
-    box.addEventListener("click", () => {
-        if (turnO) {
-            box.innerText = "O";
-            box.style.color = "#3f91a5";
-        } else {
-            box.innerText = "X";
-        }
-        turnO = !turnO;
-        updatePlayerClasses();
-
-        box.disabled = true;
-
-        checkWinner();
-    });
-});
-
-// disabling boxes after the game is finished
-let disableBoxes = () => {
-    for (let box of boxes) {
-        box.disabled = true;
-        container.classList.add("hide");
-        h1.classList.add("hide");
-        resetBtn.classList.add("hide");
-    };
+const disableBoxes = () => {
+    boxes.forEach((box) => (box.disabled = true));
 };
 
-// enabling boxes and removing their text
-let enableBoxes = () => {
-    for (let box of boxes) {
-        box.disabled = false;
-        box.innerText = "";
-    };
-};
-
-// making an arrow function to show the winner
 const showWinner = (winner) => {
     msg.innerText = `Congratulations, winner is ${winner}`;
     msgContainer.classList.remove("hide");
     back.classList.add("hide");
-    gameChange.classList.remove("hide");
-    gameChange.classList.remove("change-game-2");
     disableBoxes();
+    container.classList.add("hide");
+    h1.classList.add("hide");
+    resetBtn.classList.add("hide");
 };
 
-// making an arrow function to make a draw scenario
 const showDraw = () => {
-    msgContDraw.classList.remove("hide");
     disableBoxes();
-    resetBtn.classList.remove("hide");
-    msgContainer.classList.add("hide");
-    back.classList.add("hide");
-    resetBtn.classList.add("reset-btn");
-}
-
-// making an arrow function and using for of loop to check the winning patterns and to check the draw scenario as well
-const checkWinner = () => {
-    for(pattern of winPatterns) {
-        let pos1Val = boxes[pattern[0]].innerText;
-        let pos2Val = boxes[pattern[1]].innerText;
-        let pos3Val = boxes[pattern[2]].innerText;
-
-        if (pos1Val != "" && pos2Val != "" && pos3Val != "") {
-            if (pos1Val === pos2Val && pos2Val === pos3Val) {
-                showWinner(pos1Val);
-                playerOne.classList.add("turn");
-            }
-        };
-        if ([...boxes].every(box => box.innerText != "")){
-            showDraw();
-            playerOne.classList.add("turn");
-            resetBtn.classList.add("reset-btn-2");
-            gameChange.classList.remove("hide");
-        }
-    };
 };
-// adding new and reset button using event listener
+
+const checkWinner = () => {
+    for (const pattern of winPatterns) {
+        const [a, b, c] = pattern;
+        const pos1Val = boxes[a].innerText;
+        const pos2Val = boxes[b].innerText;
+        const pos3Val = boxes[c].innerText;
+
+        if (pos1Val && pos1Val === pos2Val && pos2Val === pos3Val) {
+            showWinner(pos1Val);
+            return;
+        }
+    }
+    if ([...boxes].every((box) => box.innerText)) {
+        showDraw();
+    }
+};
+
+boxes.forEach((box) => {
+    box.addEventListener("click", () => {
+        box.innerText = turnO ? "O" : "X";
+        box.style.color = turnO ? "#3f91a5" : "#000";
+        box.disabled = true;
+        turnO = !turnO;
+        updatePlayerClasses();
+        checkWinner();
+    });
+});
+
 newBtn.addEventListener("click", resetGame);
 resetBtn.addEventListener("click", resetGame);
-
-
