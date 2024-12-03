a6868c6a619fb7fc8717d126745f16636ec5ab0b
Hannankhan203 <ak1818375@gmail.com>
2024-12-03 20:52:26 +0500

    Fixed UI for home page

--- JS/script.js
+++ JS/script.js
@@ -1,47 +1,19 @@
 // Calling elements from html file
 let h1 = document.querySelector("h1");
 let games = document.querySelectorAll(".game");
-let modeBtn1 = document.querySelector("#mode1");
-let modeBtn2 = document.querySelector("#mode2");
+let themeCheckbox = document.querySelector("#theme-checkbox");
 let body = document.querySelector("body");
-modeBtn2.classList.add("toggle");
+let check = document.querySelector(".check");
+let botn = document.querySelector(".botn");
 
-// Setting default mode to light mode
-let mode = "Light mode";
+body.classList.add("light-mode");
+check.classList.add("light-mode");
+botn.classList.add("light-mode");
 
-// Dark mode function
-const darkMode = () => {
-  body.classList.add("body-dark-mode");
-  body.classList.remove("body");
-  games.forEach((game) => {
-    game.classList.add("dark-mode-bg");
-  });
-  modeBtn1.classList.add("toggle");
-  modeBtn2.classList.remove("toggle");
-};
+function toggleMode() {
+  body.classList.toggle("dark-mode");
+  check.classList.toggle("dark-mode");
+  botn.classList.toggle("dark-mode");
+}
 
-// Light mode function
-const lightMode = () => {
-  body.classList.remove("body-dark-mode");
-  body.classList.add("body");
-  h1.classList.remove("dark-mode-bg");
-  games.forEach((game) => {
-      game.classList.remove("dark-mode-bg");
-    });
-    modeBtn2.classList.add("toggle");
-    modeBtn1.classList.remove("toggle");
-};
-
-// Mode toggle function
-const toggleMode = () => {
-  if (mode === "Light mode") {
-    mode = "Dark mode";
-    darkMode();
-  } else {
-    mode = "Light mode";
-    lightMode();
-  }
-};
-
-modeBtn1.addEventListener("click", toggleMode);
-modeBtn2.addEventListener("click", toggleMode);
+themeCheckbox.addEventListener("click", toggleMode);
